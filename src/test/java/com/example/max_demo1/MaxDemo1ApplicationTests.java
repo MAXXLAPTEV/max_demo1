833065acba1109662daebcde89a6bff03cbb1588
MAXXLAPTEV <laptevvadimovich1@gmail.com>
2022-01-30 16:29:40 +0300

    +test

--- src/test/java/com/example/max_demo1/MaxDemo1ApplicationTests.java
+++ src/test/java/com/example/max_demo1/MaxDemo1ApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,18 +51,6 @@ class MaxDemo1ApplicationTests {
 
     @Autowired
     ObjectMapper objectMapper;
-//
-//    @Test
-//    public void postTest1() throws Exception{
-//        Car car1 = new Car();
-//        car1.setName("lada");
-//        car1.setPrice(37777);
-//        this.mockMvc.perform((RequestBuilder) post("/addCar"))
-//                .andDo(print())
-//                .andExpect(content().string(containsString("added")));
-//    }
-
-
 
 
     @Test
@@ -76,37 +65,25 @@ class MaxDemo1ApplicationTests {
         assertEquals("bmw", car.getName());
         System.out.println("car" + contentAsString);
     }
-//
-//    @Test
-//    public void postTest() throws Exception {
-//        Car car1 = new Car();
-//        car1.setPrice(2000);
-//        car1.setName("lada");
-//        MockHttpServletRequestBuilder requestBuilder = post("/addCar")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(car1));
-//        ResultActions perform = this.mockMvc.perform(requestBuilder);
-//        MvcResult mvcResult = perform.andReturn();
-//        MockHttpServletResponse response = mvcResult.getResponse();
-//        String contentAsString = response.getContentAsString();
-//        assertEquals("added", contentAsString);
 
-//		ResultActions perform = this.mockMvc.perform(MockMvcRequestBuilders
-//						.post("/addCar")
-//						.contentType(MediaType.APPLICATION_JSON))
-//				        .andExpect(content().string(containsString("added")));
-
-
-        //}
-
-//    @Test
-//    public void deleteTest() throws Exception {
-//        ResultActions perform = this.mockMvc.perform(MockMvcRequestBuilders
-//						.delete("/deleteCar")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//						.andExpect(content().string(containsString("deleted")));
-//
-//    }
-
+    @Test
+    public void postTest() throws Exception {
+        Car car1 = new Car();
+        car1.setPrice(2000);
+        car1.setName("lada");
+        MockHttpServletRequestBuilder requestBuilder = post("/car/addCar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(car1));
+        ResultActions perform = this.mockMvc.perform(requestBuilder);
+        MvcResult mvcResult = perform.andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String contentAsString = response.getContentAsString();
+        System.out.println("added" + contentAsString);
     }
+
+
+}
+
+
+
 

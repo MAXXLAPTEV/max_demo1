164c13a982875ebd34a661ad0947c4ea4eb0203b
MAXXLAPTEV <laptevvadimovich1@gmail.com>
2022-02-01 16:04:00 +0300

    +test

--- src/test/java/com/example/max_demo1/MaxDemo1ApplicationTests.java
+++ src/test/java/com/example/max_demo1/MaxDemo1ApplicationTests.java
@@ -6,14 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +22,16 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
+
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 import java.util.List;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class MaxDemo1ApplicationTests {
-
-//    @Test
-//    void contextLoads() {
-//        System.out.println("test");
-//        MyController myController = new MyController();
-//        List<Car> s = myController.getAllCars();
-//        assertTrue(s.contains("test"));
-//
-//
-//    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -61,7 +51,7 @@ class MaxDemo1ApplicationTests {
         MockHttpServletResponse response = mvcResult.getResponse();
         String contentAsString = response.getContentAsString();
         Car car = objectMapper.readValue(contentAsString, Car.class);
-        assertTrue(car.getPrice() < 3000000);
+        assertEquals(2000000 , car.getPrice());
         assertEquals("bmw", car.getName());
         System.out.println("car" + contentAsString);
     }
@@ -78,8 +68,40 @@ class MaxDemo1ApplicationTests {
         MvcResult mvcResult = perform.andReturn();
         MockHttpServletResponse response = mvcResult.getResponse();
         String contentAsString = response.getContentAsString();
-        System.out.println("added" + contentAsString);
+        System.out.println(contentAsString);
     }
+
+    @Test
+    public void CarDeleteTest() throws Exception {
+        Car car2 = new Car();
+        car2.setPrice(30000);
+        car2.setName("mini");
+        this.mockMvc.perform(delete("/car/deleteCar")
+                .header("deleted", "car " + car2))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+    @Test
+    public void AddAndDeleteCarTest() throws  Exception{
+        Car car3 = new Car();
+        car3.setPrice(40000);
+        car3.setName("kia");
+        MockHttpServletRequestBuilder requestBuilder = post("/car/addCar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(car3));
+        ResultActions perform = this.mockMvc.perform(requestBuilder);
+        MvcResult mvcResult = perform.andReturn();
+        MockHttpServletRequestBuilder requestBuilder1 = delete("/car/deleteCar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(car3));
+        ResultActions perform1 = this.mockMvc.perform(requestBuilder);
+        MvcResult mvcResult1 = perform1.andReturn();
+        MockHttpServletResponse response = mvcResult1.getResponse();
+        String contentAsString = response.getContentAsString();
+        System.out.println("car with price " + car3.getPrice() + " " + "and name " + car3.getName() + " is deleted ");
+
+    }
+
 
 
 }
